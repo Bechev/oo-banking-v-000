c7d5773f2641f66cb8aeeee9fcbff8f3ab0c7b98
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-15 02:51:17 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,7 +10,7 @@ attr_accessor :sender, :receiver, :status, :amount
   end
 
   def valid?
-    (sender.valid? && receiver.valid?)? true:false
+    (@sender.valid? && @receiver.valid?)? true:false
   end
 
   def execute_transaction
